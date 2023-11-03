1c6d55844ee6ab29e56e8a9d914b9202268358fc
shuttlersIT <itplatforms@shuttlers.ng>
2023-11-03 15:59:12 +0100

    Tidy ticketing handlers

--- handlers/ticketing.go
+++ handlers/ticketing.go
@@ -11,6 +11,9 @@ import (
 	"github.com/shuttlersIT/itsm-mvp/structs"
 )
 
+type ItTime time.Time
+
+
 //Ticket Handlers
 /*
 // Ticketing Handlers
@@ -113,7 +116,47 @@ func GetTicket(c *gin.Context) {
 }
 
 // Update a ticket by ID
-func UpdateTicket(c *gin.Context, tid int, ts string, td int, cat int, scat int, pr string, slaid int, staffid int, agentid int, due time.Time) {
+func UpdateTicketOperation(c *gin.Context, tid int, ts string, td int, cat int, scat int, pr string, slaid int, staffid int, agentid int, due ItTime, asset int, relatedTid int, tag string, site string, status string attachmentid int) int {
+	id := tid
+	subject := ts
+	description := td
+	category := cat
+	subCategory := scat
+	priority := pr
+	sLA := slaid
+	staffID := staffid
+	agentID := agentid
+	dueAt := due
+	assetID := assetid
+	relatedTicketID := relatedTid
+	tag := tag
+	site := site
+	status := status
+	attachmentID := attachmentid
+
+	// Don't forget type assertion when getting the connection from context.
+	db, ok := c.MustGet("databaseConn").(*sql.DB)
+	if !ok {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Unable to reach DB from get user handler"})
+		return 0
+	}
+	var t structs.Ticket
+	if err := c.ShouldBindJSON(&t); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return 0
+	}
+
+	_, err := db.Exec("UPDATE tickets SET subject = ?, description = ?, category_id = ?, sub_category_id = ?, priority_id, sla_id, staff_id, agent_id, due_at, asset_id, related_ticket_id, tag, site, attachment_id WHERE id = ?", t.Subject, t.Description, t.Status, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return 0
+	}
+	c.JSON(http.StatusOK, "Ticket updated successfully")
+	return t.ID
+}
+
+// Delete a ticket by ID
+func DeleteTicketOperation(c *gin.Context, tid int) {
 	id := tid
 
 	// Don't forget type assertion when getting the connection from context.
@@ -122,23 +165,35 @@ func UpdateTicket(c *gin.Context, tid int, ts string, td int, cat int, scat int,
 		c.JSON(http.StatusNotFound, gin.H{"error": "Unable to reach DB from get user handler"})
 		return
 	}
-	var t structs.Ticket
-	if err := c.ShouldBindJSON(&t); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	_, err := db.Exec("UPDATE tickets SET title = ?, description = ?, status = ? WHERE id = ?", t.Subject, t.Description, t.Status, id)
+	_, err := db.Exec("DELETE FROM tickets WHERE id = ?", id)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, "Ticket updated successfully")
+	c.JSON(http.StatusOK, "Ticket deleted successfully")
 }
 
 // Delete a ticket by ID
-func DeleteTicket(c *gin.Context, tid int) {
-	id := tid
+func UpdateTicket(c *gin.Context) {
+	//id := tid
+
+	// Don't forget type assertion when getting the connection from context.
+	db, ok := c.MustGet("databaseConn").(*sql.DB)
+	if !ok {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Unable to reach DB from get user handler"})
+		return
+	}
+	_, err := db.Exec("DELETE FROM tickets WHERE id = ?", id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, "Ticket deleted successfully")
+}
+
+// Delete a ticket by ID
+func DeleteTicket(c *gin.Context) {
+	//id := tid
 
 	// Don't forget type assertion when getting the connection from context.
 	db, ok := c.MustGet("databaseConn").(*sql.DB)
